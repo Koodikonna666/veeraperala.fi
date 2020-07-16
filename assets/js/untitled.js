5b593b47e4dd4de2564b542aef90524f2abd328d
Oskari Lehtonen <lehtonen.osku@gmail.com>
2020-07-16 16:36:25 +0300

    first commit

---
+++ assets/js/untitled.js
@@ -0,0 +1,23 @@
+
+    function kilpailu(checked){
+        if(checked == true)
+            $(".kilpailu").fadeIn();
+        else $(".kilpailu").fadeOut();
+        
+    }
+    
+    
+    function blogi(checked){
+        if(checked == true)
+            $(".blogi").fadeIn();
+        else $(".blogi").fadeOut();
+        
+    }
+     function terveys(checked){
+        if(checked == true)
+            $(".terveys").fadeIn();
+        else $(".terveys").fadeOut();
+        
+    }
+    
+    
